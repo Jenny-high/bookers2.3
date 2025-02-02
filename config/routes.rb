1aae816ce90078ba4d214e4c4519f51dd60c040c
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2025-02-02 14:06:49 +0000

    upload 0202 23:0

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  get 'home/about' => 'homes#about', as: 'about'  # 修正
+  get 'home/about' => 'homes#about', as: 'about'  
 
   resources :books, only: [:index, :show, :edit, :update, :destroy, :create, :new] do
     resources :book_comments, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_01_154132) do
+ActiveRecord::Schema.define(version: 2025_02_01_141002) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
